f480d45a73d994a36032c17e907086c1dbb8dbc1
Alex Eckardt <realalexeck@gmail.com>
2026-02-27 22:03:18 -0500

    Upgrade NextJS

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['alexeckardtgames.com'],
+  },
+};
+
+export default nextConfig;
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['alexeckardt.ca'],
-  },
-};
-
-export default nextConfig;
